0e42fb238ba6355680f4ae683a0beca876139e5b
TheBandik <shnr.arky@gmail.com>
2020-01-26 19:30:55 +1000

    Вектор + сложение векторов

---
+++ src/physics/vec2.h
@@ -0,0 +1,23 @@
+template<typename T>
+class vec2 {
+public:
+    T x, y;
+
+    vec2(T x = 0, T y = 0);
+
+    vec2 operator+(vec2 v2);
+};
+
+
+template<typename T>
+vec2<T>::vec2(T x, T y) {
+    this->x = x;
+    this->y = y;
+}
+
+template<typename T>
+vec2<T> vec2<T>::operator+(vec2 v2) {
+    x = x + v2.x;
+    y = y + v2.y;
+    return vec2(x, y);
+}
