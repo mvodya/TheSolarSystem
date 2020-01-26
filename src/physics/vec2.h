eee20d706f74daf73f5130719dfb9abd84c895f7
TheBandik <shnr.arky@gmail.com>
2020-01-26 21:39:16 +1000

    Произведение вектора на скаляр

--- src/physics/vec2.h
+++ src/physics/vec2.h
@@ -1,12 +1,13 @@
 template <typename T>
 class vec2 {
 public:
-    T x, y;
+    T x, y, n;
 
     vec2(T x = 0, T y = 0);
 
     vec2 operator+(vec2 v2);
     vec2 operator-(vec2 v2);
+    vec2 operator*(T n);
     T operator*(vec2 v2);
 };
 
@@ -35,4 +36,11 @@ T vec2<T>::operator*(vec2 v2) {
     x = x * v2.x;
     y = y * v2.y;
     return x + y;
+}
+
+template <typename T>
+vec2<T> vec2<T>::operator*(T n) {
+    x = x * n;
+    y = y * n;
+    return vec2(x, y);
 }
