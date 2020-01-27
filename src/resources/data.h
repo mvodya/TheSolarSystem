a6e6bb7b635bed48a6b49fb5bc61d4a6b3e2e70e
TheBandik <shnr.arky@gmail.com>
2020-01-27 23:43:42 +1000

    Планеты земной группы со спутниками

--- src/resources/data.h
+++ src/resources/data.h
@@ -11,6 +11,30 @@ void LOAD_DATA(std::vector<Object *> &objects, Physics *physics) {
   objects.push_back(sun);
 
   /* Планета
+   * Меркурий
+   * https://ru.wikipedia.org/wiki/Меркурий
+   */
+  Planet *mercury =
+      new Planet("Mercury", 2, sf::Color(0xffffff), sf::Color(0xffffff));
+  objects.push_back(mercury);
+  mercury->apogee.ae(0.46670079);
+  mercury->perigee.ae(0.30749951);
+  mercury->speed.km(170496);
+  mercury->Init(physics);
+
+  /* Планета
+   * Венера
+   * https://ru.wikipedia.org/wiki/Венера
+   */
+  Planet *venus =
+      new Planet("Venus", 3, sf::Color(0xffffff), sf::Color(0xffffff));
+  objects.push_back(venus);
+  venus->apogee.ae(0.72823128);
+  venus->perigee.ae(0.71843270);
+  venus->speed.km(126072);
+  venus->Init(physics);
+
+  /* Планета
    * Земля
    * https://ru.wikipedia.org/wiki/Земля
    */
@@ -24,6 +48,7 @@ void LOAD_DATA(std::vector<Object *> &objects, Physics *physics) {
 
   /* Планета
    * Земля
+   * Спутник - Луна
    * https://ru.wikipedia.org/wiki/Луна
    */
   Moon *moon = new Moon("Moon", 1.5, sf::Color(0x999999ff),
@@ -45,4 +70,32 @@ void LOAD_DATA(std::vector<Object *> &objects, Physics *physics) {
   mars->perigee.ae(1.666);
   mars->speed.km(86400);
   mars->Init(physics);
+
+  /* Планета
+   * Марс
+   * Спутник - Фобос
+   * https://ru.wikipedia.org/wiki/Фобос
+   * http://svagor.com/fobos-cel-poleta-na-mars
+   */
+  Moon *phobos =
+      new Moon("Phobos", 1, sf::Color(0x999999ff), sf::Color(0x575757ff), mars);
+  objects.push_back(phobos);
+  phobos->apogee.km(9518 * 1000);
+  phobos->perigee.km(9235 * 1000);
+  phobos->speed.km(7696 * 200);
+  phobos->Init(physics);
+
+  /* Планета
+   * Марс
+   * Спутник - Деймос
+   * https://ru.wikipedia.org/wiki/Деймос
+   * http://readings.gmik.ru/lecture/2013-KOSMICHESKIY-LIFT-FOBOS-MARS
+   */
+  Moon *deimos =
+      new Moon("Deimos", 1, sf::Color(0x999999ff), sf::Color(0x575757ff), mars);
+  objects.push_back(deimos);
+  deimos->apogee.km(23500 * 700);
+  deimos->perigee.km(23500 * 700);
+  deimos->speed.km(3600 * 200);
+  deimos->Init(physics);
 }
