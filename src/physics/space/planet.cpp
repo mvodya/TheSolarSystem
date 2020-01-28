6151a318830d22c95183d641db018c07ebba8cd1
Mark Vodyanitskiy <voderme@gmail.com>
2020-01-28 10:41:27 +1000

    Непроверенный код для колец планет

--- src/physics/space/planet.cpp
+++ src/physics/space/planet.cpp
@@ -4,12 +4,26 @@ void Planet::Draw(sf::RenderWindow* render, vec2<double> offset, double scale) {
   // Отрисовка линий центров
   if (settings->center_lines) {
     sf::Vertex line[2];
-    line[0].position = sf::Vector2f(offset.x + position.x - draw_radius * scale,
+    line[0].position =
+        sf::Vector2f(offset.x + position.x - draw_radius * scale,
                      offset.y + position.y - draw_radius * scale);
     line[0].color = sf::Color(0xffffff11);
     line[1].position = sf::Vector2f(offset.x, offset.y);
     line[1].color = sf::Color(0xffffff33);
     render->draw(line, 2, sf::Lines);
+  }
+
+  // Отрисовка кольца
+  // (при условии что оно установлено)
+  if (ring_radius) {
+    sf::CircleShape r(ring_radius * scale);
+    r.setFillColor(sf::Color(0x00000000));
+    r.setOutlineThickness(ring_tickness * scale);
+    r.setOutlineColor(ring_color);
+    offset = offset + position;
+    r.setPosition(offset.x - ring_radius * scale,
+                  offset.y - ring_radius * scale);
+    render->draw(r);
   }
 
   // Отрисовка планеты
@@ -27,7 +41,7 @@ void Planet::Draw(sf::RenderWindow* render, vec2<double> offset, double scale) {
     text.setFont(this->settings->default_font);
     text.setString(draw_name);
     text.setFillColor(sf::Color::White);
-    //text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    // text.setStyle(sf::Text::Bold | sf::Text::Underlined);
     text.setPosition(offset.x - draw_radius * scale,
                      offset.y + (draw_radius * scale) + 3);
     text.setCharacterSize(draw_radius * 2 * scale);
--- src/physics/space/planet.h
+++ src/physics/space/planet.h
@@ -13,14 +13,29 @@ class Planet : public Object {
   // Отображаемое имя
   std::string draw_name;
 
+  // Цвет кольца
+  sf::Color ring_color;
+  // Радиус кольца (0 - отключено)
+  double ring_radius = 0;
+  // Толщина кольца
+  double ring_tickness = 1;
+
  public:
   Planet(std::string name, double radius, sf::Color color_fill,
          sf::Color color_out)
       : draw_name(name),
         draw_radius(radius),
         draw_fill_color(color_fill),
-        draw_out_color(color_out){};
+        draw_out_color(color_out) {
+    // Цвет кольца по умолчанию
+    ring_color = sf::Color(0xffffff22);
+  };
 
   virtual void Draw(sf::RenderWindow* render, vec2<double> offset,
                     double scale);
+
+  // Установка кольца
+  void SetRing(sf::Color c, double radius = draw_radius + 3,
+               double tickness = 1)
+      : ring_color(c), ring_radius(radius), ring_tickness(tickness){};
 };
